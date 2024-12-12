6c17e3616f7cfe71f42022917b80254bd655b37c
Leo en laptop <leonar2zb@gmail.com>
2024-12-12 19:51:44 -0300

    Creando el esqueleto para montar las actions

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -1,8 +1,56 @@
-import type { Guitar, GuitarId } from "../types";
+import { db } from "../data/db";
+import type { cartItem, Guitar, GuitarId } from "../types";
 
 export type CartActions =
     { type: 'addToCart', payload: { item: Guitar } } |
     { type: 'removeFromCart', payload: { id: GuitarId } } |
     { type: 'increaseQuantity', payload: { id: GuitarId } } |
     { type: 'decreaseQuantity', payload: { id: GuitarId } } |
-    { type: 'clearCart' } 
+    { type: 'clearCart' }
+
+export type CartState = {
+    data: Guitar[]
+    cart: cartItem[]
+}
+
+export const initialState: CartState = {
+    data: db,
+    cart: []
+}
+
+export const cartReducer = (
+    state: CartState = initialState,
+    action: CartActions
+) => {
+    if (action.type === 'addToCart') {
+        return {
+            ...state
+        }
+    }
+
+    if (action.type === 'removeFromCart') {
+        return {
+            ...state
+        }
+    }
+
+    if (action.type === 'increaseQuantity') {
+        return {
+            ...state
+        }
+    }
+
+    if (action.type === 'decreaseQuantity') {
+        return {
+            ...state
+        }
+    }
+
+    if (action.type === 'clearCart') {
+        return {
+            ...state
+        }
+    }
+
+    return state
+}
